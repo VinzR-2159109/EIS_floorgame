d0689aa05501b89202e92c318a860e28f3b05b4e
VinzR-2159109 <vinz.roosen@student.uhasselt.be>
2024-11-27 14:11:27 +0100

    Add winner screen

---
+++ FloorGame/View/WinnerPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FloorGame.View
+{
+    public partial class WinnerScreen : Page
+    {
+        public WinnerScreen(string winnerName)
+        {
+            InitializeComponent();
+            WinnerNameText.Text = $"{winnerName} Wins!";
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
